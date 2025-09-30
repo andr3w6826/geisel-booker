46b6a52b261a211846d91d57bf6e16e8f65f665f
Andrew Chen <anc062@ucsd.edu>
2025-09-30 00:18:21 -0700

    Update run_booker.sh

--- run_booker.sh
+++ run_booker.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cd "(Put in file path of the folder)" # change to actual filepath of "geisel-booker" folder
+# change to actual filepath of "geisel-booker" folder
+cd "(Put in file path of the folder)"  # e.g. /Users/firstlastname/Downloads/geisel-booker
 
-# Run your script; change the python path if yours is different (check with: which python3)
-# First run should be headed so you can trust Duo; later you can flip to headless in code.
+
 mkdir -p logs
 /usr/bin/python3 script.py >> logs/booker.out 2>> logs/booker.err
