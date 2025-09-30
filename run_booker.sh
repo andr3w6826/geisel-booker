0ec772b158c0283b489d0743f6fa60c174b672a7
jtan10 <jeremiah.j.in.christ@gmail.com>
2025-09-30 00:13:43 -0700

    changed run booker

--- run_booker.sh
+++ run_booker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cd "/Users/andrewchen/WIP_Projects/geisel-booker"
+cd "(Put in file path of the folder)" # change to actual filepath of "geisel-booker" folder
 
 # Run your script; change the python path if yours is different (check with: which python3)
 # First run should be headed so you can trust Duo; later you can flip to headless in code.
